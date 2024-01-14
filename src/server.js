fd42d65df2a400566d93b1ff8ad2179657db17ac
nguyenle2k3 <nguyenle2k31412@gmail.com>
2024-01-14 14:28:03 +0700

    "fix cors"

--- src/server.js
+++ src/server.js
@@ -2,10 +2,28 @@ import express, { Router } from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRouters from "./routes/web";
 import bodyParser from 'body-parser';
-const cors = require('cors');
 require("dotenv").config();
 
 const app = express();
+
+// config after define app defined
+app.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Method', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Request-With, content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // config view engine
 configViewEngine(app);
@@ -13,8 +31,6 @@ configViewEngine(app);
 // config body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(cors());
 
 // init web routes
 initWebRouters(app);
