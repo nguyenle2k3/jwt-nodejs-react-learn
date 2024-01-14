aeb86ab801b2fd8ab2a5fd05c41399a5cfeff73d
nguyenle2k3 <nguyenle2k31412@gmail.com>
2024-01-14 14:27:27 +0700

    "fix cors"

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeControllers from "../controllers/homeControllers";
+import apiController from "../controllers/apiController";
 
 const router = express.Router();
 
@@ -9,13 +10,17 @@ const router = express.Router();
  */
 
 const initWebRouters = (app) => {
+    // path, handler
     router.get("/", homeControllers.handlerHelloWorld)
     router.get("/user", homeControllers.handlerUserPage)
     router.get("/users/update-user/:id", homeControllers.getUpdateUser)
-
     router.post("/users/create-user", homeControllers.handlerCreateNewUser)
     router.post("/users/delete-user/:id", homeControllers.handlerDeleteUser)
     router.post("/users/update-user", homeControllers.handlerUpdateUser)
+
+    // rest api
+    // GET - R, POST - C, PUT - U, DELETE - D
+    router.get('/api/test-api', apiController.testApi)
 
     return app.use("/", router);
 }
