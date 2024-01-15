a5f20a154f7b50d308b28f69109d733e694b8a3e
nguyenle2k3 <nguyenle2k31412@gmail.com>
2024-01-15 16:12:20 +0700

    10.1.3 - axios post data

---
+++ src/routes/api.js
@@ -0,0 +1,21 @@
+import express from "express";
+import apiController from "../controllers/apiController";
+
+const router = express.Router();
+
+/**
+ * 
+ * @param {*} app : express app
+ */
+
+const initApiRouters = (app) => {
+    // path, handler
+    // rest api
+    // GET - R, POST - C, PUT - U, DELETE - D
+    router.get('/test-api', apiController.testApi)
+    router.post('/register', apiController.handleRegister)
+
+    return app.use("/api/v1", router);
+}
+
+export default initApiRouters;
--- src/server.js
+++ src/server.js
@@ -1,46 +1,30 @@
 import express, { Router } from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRouters from "./routes/web";
-import bodyParser from 'body-parser';
+import initApiRouters from "./routes/api";
+import bodyParser from "body-parser";
+import configCors from "./config/cors";
 require("dotenv").config();
 
 const app = express();
 
-// config after define app defined
-app.use(function(req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Method', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Request-With, content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+// config cors
+configCors(app);
 
 // config view engine
 configViewEngine(app);
 
 // config body-parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // init web routes
 initWebRouters(app);
 
-// // test connection
-// import connection from "./config/connectDB";
-// connection();
+// init api
+initApiRouters(app);
 
-// 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(">>> JWT Backend is running on the port = " + PORT);
-})
+	console.log(">>> JWT Backend is running on the port = " + PORT);
+});
